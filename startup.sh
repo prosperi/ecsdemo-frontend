bb071975d78b7ede6182ed860d6e6ca620bee2f4
prosperi <prosperrriii@gmail.com>
2021-04-27 18:38:36 -0700

    Hardcode region

--- startup.sh
+++ startup.sh
@@ -81,10 +81,7 @@ fi
 if [[ -z ${zone} ]]; then
   ip_addr=$(curl -m2 -s ${ECS_CONTAINER_METADATA_URI} | jq '.Networks[].IPv4Addresses[]')
   declare -a subnets=( $(aws ec2 describe-subnets | jq .Subnets[].CidrBlock| sed ':a;N;$!ba;s/\n/ /g') )
-  echo $subnets
   for sub in "${subnets[@]}"; do
-    echo "sub----"
-    echo $sub
     if $(ruby -e "puts(IPAddr.new($sub.to_s).include? $ip_addr.to_s)") == 'true'; then
       zone=$(aws ec2 describe-subnets | jq -r ".Subnets[] | select(.CidrBlock==$sub) | .AvailabilityZone" | grep -o .$)
     fi
